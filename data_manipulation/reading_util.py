b1e9d27c6feee7884b73ce6087e33d430705fe36
Malte Weyrich <malte.weyrich@hotmail.de>
2023-09-10 13:28:19 +0200

    Finished extension of Tobis method, now it works perfectly

--- data_manipulation/reading_util.py
+++ data_manipulation/reading_util.py
@@ -86,51 +86,22 @@ def read_esm2(path_to_esm2: str, is_enzyme: bool) -> pd.DataFrame:
         return df
 
 
-def load_ml_df(path_to_enzyme_esm2: str, path_to_non_enzyme_esm2: str) -> pd.DataFrame:
-    """
-    reads both esm2 embeddings (enzymes, non_enzymes) and concatenates to main machine learning df
-    :param path_to_enzyme_esm2: Absolut path to enzyme esm2
-    :param path_to_non_enzyme_esm2: Absolut path to non_enzyme esm2
-    :return: A dataframe which can be the split to training and test data set
-    """
+# def load_ml_df(path_to_enzyme_esm2: str, path_to_non_enzyme_esm2: str) -> pd.DataFrame:
+#     """
+#     reads both esm2 embeddings (enzymes, non_enzymes) and concatenates to main machine learning df
+#     :param path_to_enzyme_esm2: Absolut path to enzyme esm2
+#     :param path_to_non_enzyme_esm2: Absolut path to non_enzyme esm2
+#     :return: A dataframe which can be the split to training and test data set
+#     """
+#
+#     enzymes = read_esm2(path_to_enzyme_esm2, True)
+#     non_enzymes = read_esm2(path_to_non_enzyme_esm2, False)
+#
+#     print(len(enzymes))
+#     print(len(non_enzymes))
+#
+#     return pd.concat([enzymes, non_enzymes])
 
-    enzymes = read_esm2(path_to_enzyme_esm2, True)
-    non_enzymes = read_esm2(path_to_non_enzyme_esm2, False)
-
-    print(len(enzymes))
-    print(len(non_enzymes))
-
-    return pd.concat([enzymes, non_enzymes])
-
-
-
-class H5Dataset(Dataset):
-    def __init__(self, h5_file: str, csv_file: str):
-        self.h5_file = h5_file
-        self.ec_numbers = self._load_ec_numbers(csv_file)
-
-        with h5py.File(self.h5_file, "r") as hdf_handle:
-            self.length = len(hdf_handle.keys())
-            self.keys = list(hdf_handle.keys())
-
-    def _load_ec_numbers(self, csv_file: str) -> dict:
-        df = pd.read_csv(csv_file)
-        ec_dict = {}
-        for _, row in df.iterrows():
-            ec_dict[row["Entry"]] = row["EC number"]
-        return ec_dict
-
-    def __len__(self):
-        return self.length
-
-    def __getitem__(self, index: int):
-        with h5py.File(self.h5_file, "r") as hdf_handle:
-            key = self.keys[index]
-            emb = torch.tensor(hdf_handle[key], dtype=torch.float32).reshape(-1)
-            header = key
-            ec_number = self.ec_numbers.get(header, pd.NA)
-
-        return emb, header, ec_number
 
 
 class H5Dataset(Dataset):
@@ -162,16 +133,45 @@ class H5Dataset(Dataset):
         return emb, header, ec_number
 
 
-def load_ml_data(path_to_esm2: str, path_tp_enzyme_csv: str, class_depth: int = 4):
+class H5Dataset(Dataset):
+    def __init__(self, h5_file: str, csv_file: str):
+        self.h5_file = h5_file
+        self.ec_numbers = self._load_ec_numbers(csv_file)
+
+        with h5py.File(self.h5_file, "r") as hdf_handle:
+            self.length = len(hdf_handle.keys())
+            self.keys = list(hdf_handle.keys())
+
+    def _load_ec_numbers(self, csv_file: str) -> dict:
+        df = pd.read_csv(csv_file)
+        ec_dict = {}
+        for _, row in df.iterrows():
+            ec_dict[row["Entry"]] = row["EC number"]
+        return ec_dict
+
+    def __len__(self):
+        return self.length
+
+    def __getitem__(self, index: int):
+        with h5py.File(self.h5_file, "r") as hdf_handle:
+            key = self.keys[index]
+            emb = torch.tensor(hdf_handle[key], dtype=torch.float32).reshape(-1)
+            header = key
+            ec_number = self.ec_numbers.get(header, pd.NA)
+
+        return emb, header, ec_number
+
+
+def load_ml_data_emb(path_to_esm2: str, path_to_enzyme_csv: str):
     """
     Reads in the embeddings and the EC numbers from the h5 file and the csv file and labels them accordingly.
     :param path_to_esm2: path to the h5 file
-    :param path_tp_enzyme_csv: path to the csv file
+    :param path_to_enzyme_csv: path to the csv file
     :param class_depth: depth of the EC numbers
     :return: X: embeddings, y: EC numbers (labels)
     """
 
-    h5_dataset = H5Dataset( path_to_esm2, path_tp_enzyme_csv)
+    h5_dataset = H5Dataset(path_to_esm2, path_to_enzyme_csv)
 
     loader = torch.utils.data.DataLoader(h5_dataset, batch_size=32, shuffle=True)
 
@@ -183,7 +183,7 @@ def load_ml_data(path_to_esm2: str, path_tp_enzyme_csv: str, class_depth: int = 
 
     for batch in loader:
         emb, _, ec_numbers = batch
-        wanted_ec_class = [".".join(ec_number.split(".")[:class_depth]) for ec_number in ec_numbers] # here we extract te ec number based on the class depth
+        wanted_ec_class = [int(ec_number.split(".")[0]) - 1 for ec_number in ec_numbers] # here we convert ec to int and do -1
         
         X.append(emb.numpy())
         y.extend(list(wanted_ec_class))
