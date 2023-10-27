ecf6438cfc9d137b831aef76c93166cd3c67e406
Sofiia <s.kozoriz@tum.de>
2023-10-27 17:39:12 +0200

    .

---
+++ KidaNN/FNN_esm.py
@@ -0,0 +1,306 @@
+import csv, os
+import itertools
+import time
+import matplotlib.pyplot as plt
+import h5py
+import numpy as np
+import sys
+import torch
+from sklearn.metrics import accuracy_score
+from sklearn.model_selection import StratifiedKFold
+from torch.utils.tensorboard import SummaryWriter
+import os
+import torch
+from torch import nn
+from torch.utils.data import DataLoader, Dataset
+from torchvision import datasets, transforms
+import torch.optim as optim
+import keyboard
+from itertools import chain
+from bootstrapping import f1_score, round_to_significance
+from bootstrapping import calculate_f1
+from reading_util import filter_unwanted_esm2, H5Dataset
+import time
+import random
+import string
+
+
+
+logger = SummaryWriter('runs/basic')
+log_t = SummaryWriter('runs/training loss')
+log_v = SummaryWriter('runs/validation loss')
+
+
+def getDevice():
+    return (
+        "cuda"
+        if torch.cuda.is_available()
+        else "mps"
+        if torch.backends.mps.is_available()
+        else "cpu"
+    )
+
+
+def computeClassWeights(labels):
+    uniqueLabels, classCounts = np.unique(ar=labels, return_counts=True)
+    class_weights = 1.0 / classCounts
+    return class_weights
+
+
+def loadData(path_to_esm2: str, path_to_enzyme_csv: str):
+    """
+    Reads in the embeddings and the EC numbers from the h5 file and the csv file and labels them accordingly.
+    :param path_to_esm2: path to the h5 file
+    :param path_to_enzyme_csv: path to the csv file
+    :return: X: embeddings, y: EC numbers (labels)
+    """
+
+    to_remove = filter_unwanted_esm2(path_to_enzyme_csv, True)
+
+    h5_dataset = H5Dataset(path_to_esm2, path_to_enzyme_csv)
+
+    loader = torch.utils.data.DataLoader(h5_dataset, batch_size=32, shuffle=False)
+
+    # Iterate over batches
+    X = []
+    y = []
+
+    t0 = time.time()
+    for batch in loader:
+        emb, header, ec_numbers = batch
+        if header not in to_remove:
+
+            ec_class = [int(ec_number.split(".")[0]) for ec_number in ec_numbers]  # here we convert ec to int and do -1
+            X.append(emb.numpy())
+            y.extend(list(ec_class))
+
+    # Convert the lists to numpy arrays
+    enzymeSequences = np.vstack(X)
+    ECHeaders = np.array(y)
+
+    t1 = time.time()
+
+    total = (t1 - t0) / 60
+
+    print(f"LOG: Data loaded in: {round(total, 3)} min")
+    print(f"LOG: ESM2 of enzymes: {len(enzymeSequences)}")
+    print(f"LOG: Labels of enzymes: {len(ECHeaders)}")
+
+    return enzymeSequences, ECHeaders
+
+
+class FNN(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.stack = nn.Sequential(
+            # nn.Dropout(0.5),
+            nn.Linear(2560, 2048, dtype=torch.float32),
+            nn.BatchNorm1d(2048),
+            nn.LeakyReLU(),
+            # nn.Dropout(0.5),
+            nn.Linear(2048, 768, dtype=torch.float32),
+            nn.BatchNorm1d(768),
+            nn.LeakyReLU(),
+            nn.Linear(768, 256, dtype=torch.float32),
+            nn.BatchNorm1d(256),
+            nn.LeakyReLU(),
+            nn.Linear(256, 7, dtype=torch.float32),
+            nn.Softmax(1),
+        )
+
+    def forward(self, x):
+        logits = self.stack(x)
+        return logits
+
+
+# HYPERPARAMS
+folds = 6
+max_epoch = 100
+
+
+class ActivationHook:
+    def __init__(self, writer, layer_name):
+        self.activations = []
+        self.writer = writer
+        self.layer_name = layer_name
+
+    def __call__(self, module, _, output):
+        self.activations.append(output)
+        # Log the activations to TensorBoard
+        self.writer.add_histogram(self.layer_name, output, global_step=1)
+
+
+class DataWrapper(Dataset):
+    def __init__(self, sequences, lables):
+        self.seq = sequences
+        self.lab = lables
+
+    def __len__(self):
+        return len(self.seq)
+
+    def __getitem__(self, idx):
+        return self.seq[idx], self.lab[idx]
+
+# Define a directory to save the model checkpoints
+checkpoint_dir = 'model_checkpoints'
+
+if not os.path.exists(checkpoint_dir):
+    os.makedirs(checkpoint_dir)
+
+def predict(model, dataloader):
+    model.eval()
+    all_outputs = []
+    with torch.no_grad():
+        for inputs, _ in dataloader:
+            outputs = model(inputs)
+            all_outputs.extend(outputs)
+    return all_outputs
+
+def bootstrap_statistic(y_true, y_pred, statistic_func, B=10_000, alpha=0.05):
+    bootstrap_scores = []
+    for _ in range(B):
+        indices = np.random.choice(len(y_pred), len(y_pred), replace=True)
+        resampled_pred = y_pred[indices]
+        resampled_true = y_true[indices]
+        score = statistic_func(resampled_true, resampled_pred)
+        bootstrap_scores.append(score)
+
+    mean_score = np.mean(bootstrap_scores)
+    standard_error = np.std(bootstrap_scores, ddof=1)
+
+    # Calculate the 95% confidence interval
+    lower_bound = np.percentile(bootstrap_scores, (alpha / 2) * 100)
+    upper_bound = np.percentile(bootstrap_scores, (1 - alpha / 2) * 100)
+
+    return mean_score, standard_error, (lower_bound, upper_bound), bootstrap_scores
+
+
+if __name__ == '__main__':
+    a, b = loadData("D:/Uni/PBL/RNN/Dataset/split30_esm2_3b.h5", 'D:/Uni/PBL/RNN/Dataset/split30.csv')
+    enzyme_sequences = np.array(a)
+    enzyme_labels = np.array(b) - 1
+
+    device = getDevice()
+    print(f"Using {device} device")
+
+    model = FNN().to(device)
+    criterion = nn.CrossEntropyLoss(
+        weight=torch.tensor(data=computeClassWeights(enzyme_labels), device=device, dtype=torch.float32))
+    optimizer = optim.Adam(model.parameters())
+    print(model)
+    # hook1 = ActivationHook(logger, "Linear_Layer_1")
+    # hook2 = ActivationHook(logger, "Linear_Layer_2")
+    # hook3 = ActivationHook(logger, "Linear_Layer_3")
+    # model.stack[0].register_forward_hook(hook1)
+    # model.stack[2].register_forward_hook(hook2)
+    # model.stack[4].register_forward_hook(hook3)
+
+    timestep = 0
+    for epoch in range(max_epoch):
+        print(f"running epoch {epoch}")
+        skf = StratifiedKFold(n_splits=folds, shuffle=True, random_state=42)
+        for fold, (tInd, vInd) in enumerate(skf.split(enzyme_sequences, enzyme_labels)):
+            trainDataset = DataWrapper(torch.as_tensor(data=enzyme_sequences[tInd], dtype=torch.float32, device=device),
+                                       torch.as_tensor(data=enzyme_labels[tInd], dtype=torch.long, device=device))
+            valDataset = DataWrapper(torch.as_tensor(data=enzyme_sequences[vInd], dtype=torch.float32, device=device),
+                                     torch.as_tensor(data=enzyme_labels[vInd], dtype=torch.long, device=device))
+            trainLoader = DataLoader(trainDataset, batch_size=32, shuffle=True, num_workers=0)
+            valLoader = DataLoader(valDataset, batch_size=32, shuffle=False, num_workers=0)
+
+            model.train()
+            for miniBatchIndex, data in enumerate(trainLoader, 0):
+                inputs, labels = data
+                optimizer.zero_grad()
+                outputs = model(inputs)
+                loss = criterion(outputs, labels)
+                loss.backward()
+                optimizer.step()
+                timestep += 32
+                log_t.add_scalar('losses', float(loss.item()), timestep)
+
+            model.eval()
+            with torch.no_grad():
+                accLoss = 0.0
+                accCount = 0
+                correctByClass = torch.zeros(7, device=device)
+                occByClass = torch.zeros(7, device=device)
+                for _, v_data in enumerate(valLoader, 0):
+                    v_inputs, v_labels = v_data
+                    v_outputs = model(v_inputs)
+                    v_loss = criterion(v_outputs, v_labels)
+                    accLoss += v_loss.item()
+                    accCount += 1
+
+                    occByClass += torch.scatter(torch.zeros(32, 7, device=device), 1, v_labels.view(-1, 1), 1).sum(
+                        dim=0)
+                    pClass = torch.argmax(input=v_outputs, dim=1)
+                    correct = torch.nonzero(torch.eq(pClass, v_labels))
+                    correctByClass += torch.scatter(torch.zeros(len(correct), 7, device=device), 1, pClass[correct],
+                                                    1).sum(dim=0)
+
+                acc = correctByClass / occByClass
+                logger.add_scalar('class-0-accuracy', acc[0], timestep)
+                logger.add_scalar('class-1-accuracy', acc[1], timestep)
+                logger.add_scalar('class-2-accuracy', acc[2], timestep)
+                logger.add_scalar('class-3-accuracy', acc[3], timestep)
+                logger.add_scalar('class-4-accuracy', acc[4], timestep)
+                logger.add_scalar('class-5-accuracy', acc[5], timestep)
+                logger.add_scalar('class-6-accuracy', acc[6], timestep)
+                log_v.add_scalar('losses', accLoss / accCount, timestep)
+
+                timestamp = time.strftime("%Y%m%d-%H%M%S")
+                random_chars = ''.join(random.choice(string.ascii_lowercase) for _ in range(4))
+                unique_filename = f'model_{timestamp}_{random_chars}.pth'
+                model_checkpoint_filename = os.path.join(checkpoint_dir, unique_filename)
+
+                # Save the model checkpoint
+                torch.save(model.state_dict(), model_checkpoint_filename)
+                print(f"Saved model checkpoint: {model_checkpoint_filename}")
+
+            if keyboard.is_pressed('Esc'):
+                break
+        if keyboard.is_pressed('Esc'):
+            break
+
+    dataset = DataWrapper(torch.as_tensor(data=enzyme_sequences, dtype=torch.float32, device=device),
+                          torch.as_tensor(data=enzyme_labels, dtype=torch.long, device=device))
+    loader = DataLoader(dataset, batch_size=32, shuffle=False)
+    scores = predict(model, loader)
+
+    predicted_classes = list(itertools.chain([torch.argmax(input=score, dim=0).cpu().tolist() for score in scores]))
+
+    # Metrics: F1 Score and Accuracy
+    f1 = f1_score(enzyme_labels, predicted_classes, average='weighted')
+    accuracy = accuracy_score(enzyme_labels, predicted_classes)
+
+    print(f"F1 Score: {f1:.4f}")
+    print(f"Accuracy: {accuracy:.4f}")
+
+    mean_score, standard_error, bounds, bootstrap_scores = bootstrap_statistic(np.array(enzyme_labels), np.array(predicted_classes), calculate_f1)
+
+    # Set the range of values you want to plot (e.g., between 0.70 and 0.74)
+    min_value = 0.8
+    max_value = 0.99
+
+    # Filter data within the specified range
+    filtered_data = [x for x in bootstrap_scores if min_value <= x <= max_value]
+
+    # Create the histogram
+    plt.hist(filtered_data, bins=20, edgecolor='green')
+
+    # Set the title and labels
+    plt.title('Close-up Distribution Plot')
+    plt.xlabel('Values')
+    plt.ylabel('Frequency')
+
+    # Show the plot
+    plt.show()
+
+
+    initial_f1 = calculate_f1(enzyme_labels, predicted_classes)
+    rounded_mean_f1, rounded_se_f1 = round_to_significance(mean_score, standard_error)
+    print(f"  - Initial F1 Score: {initial_f1:.2f}")
+    print(f"  - Mean ± SE: {rounded_mean_f1} ± {rounded_se_f1}")
+    print(f"  - 95% CI: [{bounds[0]:.2f}, {bounds[1]:.2f}]")
+
+    print("Finished")
---
+++ KidaNN/reading_util.py
@@ -0,0 +1,384 @@
+import h5py
+import numpy as np
+import pandas as pd
+import torch
+from torch.utils.data import Dataset
+import time
+
+
+def read_esm2(path_to_esm2: str, is_enzyme: bool, ignore_headers: list) -> pd.DataFrame:
+    """
+    :param path_to_esm2: Absolute path to esm2 file
+    :return: A dataframe
+    """
+    with h5py.File(path_to_esm2) as hdf_handle:
+        headers = []
+        embeddings = []
+
+        for header, emb in hdf_handle.items():
+            if header not in ignore_headers:
+                headers.append(header)
+                embeddings.append(np.array(list(emb)))
+
+        df = pd.DataFrame(data={"Entry": headers, "Embedding": embeddings})
+
+        if is_enzyme:
+            df["Label"] = 1
+        else:
+            df["Label"] = -1
+
+        return df
+
+
+def read_fasta_to_df(file: str) -> pd.DataFrame:
+    """
+    method for reading in fasta file and converting it to a pandas dataframe
+    :param file: Abs path to fasta file
+    :return: A df with all seqs and ids
+    """
+    fasta_dict = {}
+
+    with open(file, "r") as f:
+        lines = f.readlines()
+
+    current_key = ""
+    for line in lines:
+        line = line.strip("\n")
+        if line[0] == ">":
+            fasta_dict[line[1::]] = ""
+            current_key = line[1::]
+        else:
+            fasta_dict[current_key] += line
+
+    f.close()
+
+
+    # Set the first row as column names
+    df = pd.DataFrame(fasta_dict.items(), columns=['Entry', 'Sequence'])
+
+    return df
+
+
+def filter_unwanted_seqs(df: pd.DataFrame, enzymes: bool) -> pd.DataFrame:
+    """
+    :param df: A dataframe containing either enzymes or non enzymes
+    :param enzymes: If we pass a df containing enzymes we also need to filter out multifunctional enzymes
+    :return: A filtered dataframe
+    """
+
+    # remove unwanted aas
+    df = df[~df['Sequence'].str.contains('O')]
+    df = df[~df['Sequence'].str.contains('U')]
+
+    # for enzymes remove multifunctional enzymes
+    if enzymes:
+        multifunc_enzymes = df[df['EC number'].str.contains(';')]
+        to_remove = []
+        for multi_ec in multifunc_enzymes['EC number']:
+            ec = multi_ec.split(';')
+            test_set = set()
+
+            for i in range(len(ec)):
+               test_set.add(ec[i][0])
+
+            if len(test_set) > 1: # If this len(set) != 1 we have a multi func enzyme of different main classes
+                to_remove.append(ec)
+
+        df = df[~df['EC number'].isin(to_remove)]
+
+    else:
+        df = df[
+            df["Sequence"].apply(len) <= 1022]  # if were working with non_enzymes we need to limit the sequence length
+
+    return df
+
+
+def filter_unwanted_esm2(path_to_csv: str, is_enzyme: bool):
+    """
+    :param path_to_csv: Absolute path to csv file
+    :param is_enzyme: If we pass enzymes we also need to filter out multifunctional enzymes
+    :return: A list of headers to ignore when reading in the esm2 embeddings
+    """
+    if is_enzyme:
+        df = pd.read_csv(path_to_csv, sep=",")
+    else:
+        df = read_fasta_to_df(path_to_csv)
+    to_remove = []
+
+    remove_O = df[df['Sequence'].str.contains('O')]["Entry"]
+    remove_U = df[df['Sequence'].str.contains('U')]["Entry"]
+
+    print(f"LOG: {len(remove_O)} Sequences with aa O in {path_to_csv}")
+    print(f"LOG: {len(remove_U)} Sequences with aa U in {path_to_csv}")
+
+    to_remove.extend(remove_O.to_list())
+    to_remove.extend(remove_U.to_list())
+
+    # for enzymes remove multifunctional enzymes
+    if is_enzyme:
+        multifunc_enzymes = df[df['EC number'].str.contains(';')]
+        remove_multif = []
+        for index, row in multifunc_enzymes.iterrows():
+
+            ec = row["EC number"].split(';') # split the ec numbers by ; meaning ec = ["1.2.3.4", "2.2.3.44"] for 1.2.3.4;2.2.3.44. This would be removed
+            header = row["Entry"]
+           
+            test_set = set()
+            for i in range(len(ec)):
+               test_set.add(int(ec[i][0]))
+
+            if len(test_set) > 1: # If this len(set) != 1 we have a multi func enzyme of different main classes
+                remove_multif.append(header)
+
+        to_remove.extend(remove_multif)
+        print(f"LOG: {len(remove_multif)} multifunctional enzymes with diff ec main classes in {path_to_csv}")
+
+    else:
+        remove_seq_cutoff = df[df["Sequence"].apply(len) > 1022]["Entry"]  # if were working with non_enzymes we need to limit the sequence length
+        print(f"LOG: {len(remove_seq_cutoff)} non enzymes are longer than 1022 cutoff")
+        to_remove.extend(remove_seq_cutoff.to_list())
+
+    print(f"LOG: {len(to_remove)} entries will be ignored")
+    return to_remove
+
+
+class H5Dataset(Dataset):
+    def __init__(self, h5_file: str, csv_file: str):
+        self.h5_file = h5_file
+        self.ec_numbers = self._load_ec_numbers(csv_file)
+
+        with h5py.File(self.h5_file, "r") as hdf_handle:
+            self.length = len(hdf_handle.keys())
+            self.keys = list(hdf_handle.keys())
+
+    def _load_ec_numbers(self, csv_file: str) -> dict:
+        df = pd.read_csv(csv_file)
+        ec_dict = {}
+        for _, row in df.iterrows():
+            ec_dict[row["Entry"]] = row["EC number"]
+        return ec_dict
+
+    def __len__(self):
+        return self.length
+
+    def __getitem__(self, index: int):
+        with h5py.File(self.h5_file, "r") as hdf_handle:
+            key = self.keys[index]
+            emb = torch.tensor(hdf_handle[key], dtype=torch.float32).reshape(-1)
+            header = key
+            ec_number = self.ec_numbers.get(header, pd.NA)
+
+        return emb, header, ec_number
+
+
+class H5ESM2(Dataset):
+    def __init__(self, h5_file: str):
+        self.h5_file = h5_file
+
+        with h5py.File(self.h5_file, "r") as hdf_handle:
+            self.length = len(hdf_handle.keys())
+            self.keys = list(hdf_handle.keys())
+
+    def __len__(self):
+        return self.length
+
+    def __getitem__(self, index: int):
+        with h5py.File(self.h5_file, "r") as hdf_handle:
+            key = self.keys[index]
+            emb = torch.tensor(hdf_handle[key], dtype=torch.float32).reshape(-1)
+            header = key
+
+        return emb, header
+
+
+def load_ml_data_emb(path_to_esm2: str, path_to_enzyme_csv: str):
+    """
+    Reads in the embeddings and the EC numbers from the h5 file and the csv file and labels them accordingly.
+    :param path_to_esm2: path to the h5 file
+    :param path_to_enzyme_csv: path to the csv file
+    :return: X: embeddings, y: EC numbers (labels)
+    """
+
+    to_remove = filter_unwanted_esm2(path_to_enzyme_csv, True)
+
+    h5_dataset = H5Dataset(path_to_esm2, path_to_enzyme_csv)
+
+    loader = torch.utils.data.DataLoader(h5_dataset, batch_size=32, shuffle=False)
+
+    # Iterate over batches
+    X = []
+    y = []
+
+    t0 = time.time()
+    for batch in loader:
+        emb, header, ec_numbers = batch
+        if header not in to_remove:
+
+            ec_class = [int(ec_number.split(".")[0]) - 1 for ec_number in ec_numbers]  # here we convert ec to int and do -1
+            X.append(emb.numpy())
+            y.extend(list(ec_class))
+
+    # Convert the lists to numpy arrays
+    X = np.vstack(X)
+    y = np.array(y)
+
+    t1 = time.time()
+
+    total = (t1 - t0) / 60
+
+    print(f"LOG: Data loaded in: {round(total, 3)} min")
+    print(f"LOG: ESM2 of enzymes: {len(X)}")
+    print(f"LOG: Labels of enzymes: {len(y)}")
+
+    return X, y
+
+
+def load_non_enz_esm2(non_enzymes_fasta_path: str, non_enzymes_esm2_path: str):
+    """
+    Used for reading in esm2 embeddings of non_enzymes, since we don't have a .csv file
+    Filters sequences longer than cutoff at 1022, sequences containing either O od U as aa
+    :param non_enzymes_fasta_path: Path to non_enzyme_fasta
+    :param non_enzymes_esm2_path: Path to non_enzyme_esm2
+    :return: X_neg, y_neg
+    """
+
+    non_enz_to_remove = filter_unwanted_esm2(non_enzymes_fasta_path, False)
+
+    h5_esm2 = H5ESM2(non_enzymes_esm2_path)
+
+    loader = torch.utils.data.DataLoader(h5_esm2, batch_size=32, shuffle=False)
+
+    # Iterate over batches
+    X_neg = []
+
+    t0 = time.time()
+
+    for batch in loader:
+        emb, header = batch
+        if header not in non_enz_to_remove:
+            X_neg.append(emb.numpy())
+
+    t1 = time.time()
+
+    # Convert the lists to numpy arrays
+    X_neg = np.vstack(X_neg)
+
+    y_neg = [7 for _ in range(len(X_neg))]  # adding labels for non enzymes:
+    # (0-6 → enzyme; 7 → non_enzyme)
+    y_neg = np.array(y_neg)
+
+    total = (t1 - t0) / 60
+
+    print(f"LOG: Non Enzymes data loaded in: {round(total, 3)} min")
+    print(f"LOG: ESM2 of non enzymes: {len(X_neg)}")
+    print(f"LOG: Labels of non enzymes: {len(y_neg)}")
+
+    return X_neg, y_neg
+
+
+
+def load_and_extract_2nd_class(path_to_esm2: str, path_to_enzyme_csv: str, wanted_ec_class: int):
+    """
+    Reads in the embeddings and the EC numbers from the h5 file and the csv file and labels them accordingly based on the wanted_ec_class.
+    :param path_to_esm2: path to the h5 file
+    :param path_to_enzyme_csv: path to the csv file
+    :param wanted_ec_class: The main class of which we want to extract the second class
+    :return: X: embeddings, y: EC numbers (labels) ready to use for cnn
+    :return: sec_to_label: dict mapping the second class to a label (we use this for the output layer in cnn, since it has to be continuous (e.g. 0,1,2,3,4,5,6,7))
+    :return: label_to_sec: dict mapping the label to the second class (we need this inorder to map the predictions back to the second class)
+    """
+
+    to_remove = filter_unwanted_esm2(path_to_enzyme_csv, True)
+
+    h5_dataset = H5Dataset(path_to_esm2, path_to_enzyme_csv)
+
+    loader = torch.utils.data.DataLoader(h5_dataset, batch_size=32, shuffle=False)
+
+    # Iterate over batches
+    X = []
+    y = []
+
+    t0 = time.time()
+
+    for batch in loader:
+        emb, header, ec_numbers = batch
+        if header not in to_remove:
+            ec_classes = [int(ec_number.split(".")[0]) for ec_number in ec_numbers]  # extract the main class, then we check if it is the wanted class
+            sec_ec_classes = [int(ec_number.split(".")[1]) for ec_number in ec_numbers]
+            for index, ec_class in enumerate(ec_classes):
+                if ec_class == wanted_ec_class:
+                    sec_ec_class = sec_ec_classes[index]
+                    X.append(emb[index].numpy()) # we append the embedding of the wanted class
+                    y.append(sec_ec_class)
+
+    # Convert the lists to numpy arrays
+    X = np.vstack(X)
+    # y = np.array(y)
+
+    # Now we create a dict numbering the ec classes since we use them as labels for our output layer in cnn
+    label_to_sec = {i: sec_ec_class for i, sec_ec_class in enumerate(set(y))}
+    sec_to_label = {sec_ec_class: i for i, sec_ec_class in enumerate(set(y))}
+
+    y_cnn_labels = [sec_to_label[sec_ec_class] for sec_ec_class in y]
+    y_cnn_labels = np.array(y_cnn_labels)
+
+    t1 = time.time()
+
+    total = (t1 - t0) / 60
+
+    print(f"LOG: Data loaded in: {round(total, 3)} min")
+    print(f"LOG: ESM2 of enzymes: {len(X)}")
+    print(f"LOG: Labels of enzymes: {len(y)}")
+
+    return X, y_cnn_labels, sec_to_label, label_to_sec
+
+
+def load_all_sub_classes(path_to_esm2: str, path_to_enzyme_csv: str, allowed_labels: dict):
+    """
+    Method which loads enzyme labels with a depth of 2 (1.1, 1.2, ...) and labels them accordingly.
+    """
+
+    to_remove = filter_unwanted_esm2(path_to_enzyme_csv, True)
+
+    h5_dataset = H5Dataset(path_to_esm2, path_to_enzyme_csv)
+
+    loader = torch.utils.data.DataLoader(h5_dataset, batch_size=32, shuffle=False)
+
+    # Iterate over batches
+    X = []
+    y = []
+
+    t0 = time.time()
+
+    for batch in loader:
+        emb, header, ec_numbers = batch
+        if header not in to_remove:
+
+            # Making sure to get the fist and second ec class and then concatenate them back together to float
+            ec_classes = [ec_number.split(".")[0] + "." + ec_number.split(".")[1] for ec_number in ec_numbers]
+
+            # Converting classes to labels
+            ec_labels = [allowed_labels[ec_number] for ec_number in ec_classes]
+
+            X.extend(emb) # we append the embedding of the wanted class
+            y.extend(ec_labels)
+
+    # X = X.extend(X)
+    # y = y.extend(y)
+
+    # Convert the lists to numpy arrays
+    X = np.vstack(X)
+    y = np.array(y)
+
+    # Now we create a dict numbering the ec classes since we use them as labels for our output layer in cnn
+    label_to_sec = {i: sec_ec_class for i, sec_ec_class in enumerate(allowed_labels.keys())}
+
+    t1 = time.time()
+
+    total = (t1 - t0) / 60
+
+    print(f"LOG: Data loaded in: {round(total, 3)} min")
+    print(f"LOG: ESM2 of enzymes: {len(X)}")
+    print(f"LOG: Labels of enzymes: {len(y)}")
+
+    return X, y, label_to_sec
